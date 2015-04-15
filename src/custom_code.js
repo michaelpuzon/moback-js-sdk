a96009600a0a11f4cbcb8225a9bee04fd58c3a18
Mike Puzon <michaelp@dobango.com>
2015-04-14 17:31:54 -0700

    custom code define function is now static

--- src/custom_code.js
+++ src/custom_code.js
@@ -2,23 +2,25 @@
  * Custom code allows you to define functions that are to be executed in the cloud.
  */
 moback.customCode = function () {
-  var mobackCloudFunctions = [];
-
-  /**
-   * Define a new custom code function.
-   * @param {String} name A unique function name
-   * @param {Function} callback Callback should be in function(request, response) format
-   */
-  this.define = function (name, callback) {
-    mobackCloudFunctions.push({name: name, callback: callback})
-  };
+  moback.customCode.mobackCloudFunctions = [];
 
   /**
    * Returns the current cloud functions defined
    * @returns {Array}
    */
   this.cloudFunctions = function(){
-    return mobackCloudFunctions;
+    return moback.customCode.mobackCloudFunctions;
   };
+};
 
+/**
+ * Define a new custom code function.
+ * @param {String} name A unique function name
+ * @param {Function} callback Callback should be in function(request, response) format
+ */
+moback.customCode.define = function (name, callback) {
+  if(moback.customCode.mobackCloudFunctions == undefined){
+    moback.customCode.mobackCloudFunctions = [];
+  }
+  moback.customCode.mobackCloudFunctions.push({name: name, callback: callback})
 };
