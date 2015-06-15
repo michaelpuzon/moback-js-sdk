9a0f0b76ee89dea98c72635ff9375bb862fcd424
Mike Puzon <michaelp@dobango.com>
2015-06-15 13:58:41 -0700

    rewrote the ajax helper library, since previous one did not support threading

--- src/helper/ajax.js
+++ src/helper/ajax.js
@@ -1,23 +1,7 @@
 /*
  Copyright (c) 2008 Stefan Lange-Hegermann
 
- Permission is hereby granted, free of charge, to any person obtaining a copy
- of this software and associated documentation files (the "Software"), to deal
- in the Software without restriction, including without limitation the rights
- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- copies of the Software, and to permit persons to whom the Software is
- furnished to do so, subject to the following conditions:
-
- The above copyright notice and this permission notice shall be included in
- all copies or substantial portions of the Software.
-
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- THE SOFTWARE.
+ taken from https://developer.mozilla.org/en-US/docs/AJAX/Getting_Started
  */
 /*
  * method - GET, POST PUT, DELETE
@@ -25,59 +9,55 @@
  * headers - additional headers, optional
  * postData - postBody, optional
  * */
-function microAjax(method, url, callbackFunction, headers, postData, fileMode)
-{
-  this.bindFunction = function (caller, object) {
-    return function() {
-      return caller.apply(object, [object]);
-    };
-  };
-
-  this.stateChange = function () {
-    if (this.request.readyState == 4){
-      var result = JSON.parse(this.request.responseText);
-      this.callbackFunction(result);
+function microAjax(method, url, callbackFunction, headers, postData, fileMode) {
+  var httpRequest, postBody;
+  if (window.XMLHttpRequest) { // Mozilla, Safari, ...
+    httpRequest = new XMLHttpRequest();
+  } else if (window.ActiveXObject) { // IE
+    try {
+      httpRequest = new ActiveXObject("Msxml2.XMLHTTP");
     }
-  };
+    catch (e) {
+      try {
+        httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
+      }
+      catch (e) {}
+    }
+  }
 
-  this.getRequest = function() {
-    if (window.ActiveXObject)
-      return new ActiveXObject('Microsoft.XMLHTTP');
-    else if (window.XMLHttpRequest)
-      return new XMLHttpRequest();
+  if (!httpRequest) {
+    console.log('Giving up :( Cannot create an XMLHTTP instance');
     return false;
-  };
+  }
+
+  function processRequest(){
+    if (httpRequest.readyState === 4) {
+      var result = JSON.parse(httpRequest.responseText);
+      callbackFunction(result);
+    }
+  }
 
   if(fileMode){
-    this.postBody = postData;
+    postBody = postData;
   } else {
-    this.postBody = (postData) ? JSON.stringify(postData) : "";
+    postBody = (postData) ? JSON.stringify(postData) : "";
   }
 
-  this.callbackFunction=callbackFunction;
-  this.url=url;
-  this.request = this.getRequest();
-
-  if(this.request) {
-    var req = this.request;
-    req.onreadystatechange = this.bindFunction(this.stateChange, this);
-    //
-
-    if (this.postBody!=="") {
-      req.open(method, url, true);
-      req.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-    } else {
-      req.open(method, url, true);
-    }
-    if(!fileMode){
-      req.setRequestHeader("Content-Type", "application/json");
-    }
-    /*add any additional headers here*/
-    if(headers){
-      for(var key in headers){
-        req.setRequestHeader(key, headers[key]);
-      }
-    }
-    req.send(this.postBody);
+  httpRequest.onreadystatechange = processRequest;
+  httpRequest.open(method, url, true);
+  if (postBody !== "") {
+    httpRequest.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+  } else {
+    httpRequest.open(method, url, true);
   }
+  if(!fileMode){
+    httpRequest.setRequestHeader("Content-Type", "application/json");
+  }
+  /*add any additional headers here*/
+  if(headers){
+    for(var key in headers){
+      httpRequest.setRequestHeader(key, headers[key]);
+    }
+  }
+  httpRequest.send(postBody);
 }
